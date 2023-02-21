497b0053bf5410a8b4d566750569d3cde500311b
fancier <fancier@proton.me>
2023-02-21 21:50:39 +0400

    init

---
+++ src/app.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <Text>App</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
